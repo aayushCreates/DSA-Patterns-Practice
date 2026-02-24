fe051d24c18e59724d3bc82ab8cd2ac0b932d562
AyushAJS <jangidayush89@gmail.com>
2026-02-24 23:42:57 +0530

    binary_search implementation is added

---
+++ binary_search/binary_search_implementation.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int binarySearch_1D_arr(vector<int> arr, int target) {
+    int start = 0;
+    int end = arr.size() - 1;
+    
+    while(start <= end) {
+        int mid = start + (start - end) / 2;
+        
+        if(arr[mid] == target) {
+            return mid;
+        }else if(arr[mid] > target) {       //  target in left side
+            end = mid - 1;
+        }else {
+            start = mid + 1;                //  target in right side
+        }
+    }
+
+    return  -1;             //  target "NOT FOUND"
+}
